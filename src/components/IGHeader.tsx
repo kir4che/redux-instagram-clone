7bc3d08388686a1a98f9ff4f853126c55e2e76d8
kir4che <mollydcxxiii@gmail.com>
2022-09-19 00:15:23 +0800

    Update IGHeader.tsx

--- src/components/IGHeader.tsx
+++ src/components/IGHeader.tsx
@@ -5,7 +5,7 @@ import IGNav from './IGNav'
 const IGHeader: React.FC = () => {
 	return (
 		<header className='sticky top-0 bg-white border-b-[1px] border-gray-300'>
-			<div className='flex justify-between items-center max-w-[967px] h-[60px] mx-auto px-2 box-border tablet:px-4'>
+			<div className='flex justify-between items-center max-w-[967px] h-[60px] mx-auto px-2 tablet:px-4'>
 				<Link to='/' className='w-[110px]'>
 					<img src='/images/logo.svg' alt='' />
 				</Link>
