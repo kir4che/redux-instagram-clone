6a622593c16226607058974824e6da8c7cbda8ae
kir4che <mollydcxxiii@gmail.com>
2022-09-19 01:33:22 +0800

    Update homeServices.ts

--- src/services/homeServices.ts
+++ src/services/homeServices.ts
@@ -21,7 +21,7 @@ type IGStory = {
 // 建立 RTKQ 的 API service，並取名為 homeApi。
 export const homeApi = createApi({
 	reducerPath: 'homeApi',
-	baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3004/' }),
+	baseQuery: fetchBaseQuery({ baseUrl: 'https://fake-ins-api.herokuapp.com/' }),
 	endpoints: (builder) => ({
 		getIGPosts: builder.query<IGPost[], number | 'all'>({
 			query: (id) => {
