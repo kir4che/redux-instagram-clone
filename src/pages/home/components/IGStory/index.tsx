0003ffa21bae97a966ca5fe65cc27a54b21cc7e0
kir4che <mollydcxxiii@gmail.com>
2022-09-19 00:19:19 +0800

    Update index.tsx

--- src/pages/home/components/IGStory/index.tsx
+++ src/pages/home/components/IGStory/index.tsx
@@ -6,7 +6,7 @@ const IGStory: React.FC = () => {
 	const { data, isLoading } = useGetIGStoiesQuery('all')
 
 	return (
-		<div className='flex items-center w-full h-[118px] mb-4 pt-2 px-5 space-x-4 bg-white border-[1px] border-gray-300 rounded-lg box-border overflow-x-auto overflow-y-hidden no-scrollbar'>
+		<div className='flex items-center w-full h-[118px] mb-4 pt-2 px-5 space-x-4 bg-white border-[1px] border-gray-300 rounded-lg overflow-x-auto overflow-y-hidden no-scrollbar'>
 			{isLoading && (
 				<div className='flex justify-center w-full'>
 					<Loading />
